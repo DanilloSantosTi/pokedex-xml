229c3b042998b4410c46eadef995345acb322cd6
DanilloSantosTi <danillomdsti@gmail.com>
2023-12-09 03:08:54 -0300

    [main] feature - create navigation

--- app/src/main/java/com/example/pokedex_xml/presenter/view/MainActivity.kt
+++ app/src/main/java/com/example/pokedex_xml/presenter/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.pokedex_xml
+package com.example.pokedex_xml.presenter.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokedex_xml.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
